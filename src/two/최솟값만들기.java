fd6ec8516441f71a526c1107ed7390780adb8059
lcw3176 <lcw3176@naver.com>
2023-07-03 17:56:49 +0900

    최솟값 만들기

---
+++ src/two/최솟값만들기.java
@@ -0,0 +1,23 @@
+package two;
+
+import java.util.*;
+
+public class 최솟값만들기 {
+
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        PriorityQueue<Integer> a = new PriorityQueue<>();
+        PriorityQueue<Integer> b = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < A.length; i++) {
+            a.add(A[i]);
+            b.add(B[i]);
+        }
+
+        while (!a.isEmpty()) {
+            answer += a.poll() * b.poll();
+        }
+
+        return answer;
+    }
+}
